b8669b564e366f97e1167eec80f52da8b77a3c3a
unknown <saiprakashbeemari@gmail.com>
2022-01-31 11:32:23 +0530

    Practise program

---
+++ src/com/bl/Bignumber.java
@@ -0,0 +1,25 @@
+package com.bl;
+
+public class Bignumber {
+public static void main(String args[])
+{
+	int a,b,c;
+	a=30;
+	b=20;
+	c=10;
+	if(a>b && a>c)
+	{
+		System.out.println("A is big");
+	
+	}
+	else if(b>c)
+	{
+		System.out.println("B is big");
+	}
+	else
+	{
+	System.out.println("C is big");
+	}
+}
+
+}
---
+++ src/com/bl/Hello.java
@@ -0,0 +1,15 @@
+package com.bl;
+
+public class Hello 
+{
+	public static void main(String args[])
+	{
+		if(System.out.printf(("Hello World")==null))
+	
+	{
+			
+			}
+	}
+	
+
+}
